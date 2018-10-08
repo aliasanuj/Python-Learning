727818a8c0a7cf44a0761ceb78a6384e6d556a4d
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-10-08 18:01:36 +0530

    Update coroutine.py

--- Git Python/Python core/coroutine.py
+++ Git Python/Python core/coroutine.py
@@ -1,1 +1,34 @@
 #coroutine.py
+===========================
+def print_name(prefix): 
+    print("Searching prefix:{}".format(prefix)) 
+    while True: 
+        name = (yield) 
+        if prefix in name: 
+            print(name) 
+corou = print_name("Dear") 
+corou.__next__() 
+corou.send("Atul") 
+corou.send("Dear Atul")
+corou.close()
+#Searching prefix:Dear
+#Dear Atul
+======================
+def print_name(prefix): 
+    print("Searching prefix:{}".format(prefix)) 
+    try :  
+        while True: 
+                name = (yield) 
+                if prefix in name: 
+                    print(name) 
+    except GeneratorExit: 
+            print("Closing coroutine!!") 
+corou = print_name("Dear") 
+corou.__next__() 
+corou.send("Atul") 
+corou.send("Dear Atul") 
+corou.close() 
+#Searching prefix:Dear
+#Dear Atul
+#Closing coroutine!!
+========================
