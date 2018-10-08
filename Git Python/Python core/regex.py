bf42a55f46efd5c224c578ad5bc94900ed8661b2
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-10-08 19:22:05 +0530

    Update regex.py

--- Git Python/Python core/regex.py
+++ Git Python/Python core/regex.py
@@ -177,3 +177,30 @@ match = re.findall(regex, string)
 print(match)
 #['123456789', '987654321']
 ===========================================
+import re
+
+line = "Cats are smarter than dogs"
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+if matchObj:
+   print ("matchObj.group() : ", matchObj.group())
+   print ("matchObj.group(1) : ", matchObj.group(1))
+   print ("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print("No match!!")
+#matchObj.group() :  Cats are smarter than dogs
+#matchObj.group(1) :  Cats
+#matchObj.group(2) :  smarter
+================================================
+import re
+phone = "2004-959-559 # This is Phone Number"
+# Delete Python-style comments
+num = re.sub(r'#.*$', "", phone)
+print ("Phone Num : ", num)
+# Remove anything other than digits
+num = re.sub(r'\D', "", phone)    
+print ("Phone Num : ", num)
+#Phone Num :  2004-959-559 
+#Phone Num :  2004959559
+=================================================================
+https://www.tutorialspoint.com/python/python_reg_expressions.htm
+==================================================================
