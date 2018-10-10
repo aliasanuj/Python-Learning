92d85d00c4b44400bd2f218fa76d7b814e7445ab
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-10-10 15:10:21 +0530

    Update python oop 02.py

--- Git Python/python oops/python oop 02.py
+++ Git Python/python oops/python oop 02.py
@@ -483,7 +483,7 @@ class Demo:
     def __check(self):
         return " Demo's check "
     def display(self):
-        print(self.check())
+        print(self.__check())
 class Demo_Derived(Demo):
     def __check(self):
         return " Derived's check "
