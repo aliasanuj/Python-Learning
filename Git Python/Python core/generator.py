52cc2a6befbbbb02abdbc57d08c0e99f54d1064d
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-10-11 12:03:30 +0530

    Update generator.py

--- Git Python/Python core/generator.py
+++ Git Python/Python core/generator.py
@@ -132,5 +132,84 @@ print(next(obj))
 #3
 #4
 ============================
-
+def my_gen():
+    n = 1
+    # Generator function contains yield statements
+    yield n
+    n += 1
+    yield n
+    n += 1
+    yield n
+for i in my_gen():
+  print(i)
+#1
+#2
+#3
+===========================================
+def simpleGeneratorFun():
+    yield 1
+    yield 2
+    yield 3
+# x is a generator object
+x = simpleGeneratorFun()
+# Iterating over the generator object using next
+print(next(x));  # In Python 3, __next__()
+print(x.__next__());  # In Python 3, __next__()
+print(next(x));  # In Python 3, __next__()
+#1
+#2
+#3
+=================================================
+def generator():
+    n = 1
+    print("value of n is :",n)
+    yield n
+    n = n+1
+    print("value of n is :", n)
+    yield n
+a = generator()
+print(next(a))
+#value of n is : 1
+#1
+==================================================
+def my_gen():
+    n = 1
+    print('This is printed first')
+    # Generator function contains yield statements
+    yield n
+    n += 1
+    print('This is printed second')
+    yield n
+    n += 1
+    print('This is printed at last')
+    yield n
+a = my_gen()
+next(a)
+print(next(a))
+next(a)
+#This is printed first
+#This is printed second
+#2
+#This is printed at last
+===============================================
+def simpleGeneratorFun():
+    yield 1
+    yield 2
+    yield 3
+# Driver code to check above generator function
+for value in simpleGeneratorFun():
+    print(value)
+print("another way :")
+obj = simpleGeneratorFun()
+print(next(obj))
+print(next(obj))
+print(next(obj))
+#1
+#2
+#3
+#another way :
+#1
+#2
+#3
+=================================================
 
