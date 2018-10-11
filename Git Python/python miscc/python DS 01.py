d89a1ee9daa5583d484cb5c7336b7c1efbb501f1
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-10-11 15:13:49 +0530

    Update python DS 01.py

--- Git Python/python miscc/python DS 01.py
+++ Git Python/python miscc/python DS 01.py
@@ -1,1 +1,7 @@
-Python learning
+#Python learning
+=========================================
+a = "anuj kumar frt 154 'ds"
+b = a.split()
+print(b)
+#['anuj', 'kumar', 'frt', '154', "'ds"]
+=========================================
