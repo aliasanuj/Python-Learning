8a4a6a9e73decee771061bb19c12a0b93618fc72
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-01 22:33:18 +0530

    Update reduce_function.py

--- Git Python/Python core/reduce_function.py
+++ Git Python/Python core/reduce_function.py
@@ -113,7 +113,7 @@ result = functools.reduce(lambda x,y : x+y,list1)
 print(result)
 #21
 ========================
-import functools
+import functools 
 lis = [1, 3, 5, 6, 2, ]
 print("The sum of the list elements is : ", end="")
 print(functools.reduce(lambda a, b: a + b, lis))
