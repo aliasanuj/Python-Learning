f30cb805a54e8a5f4fc6a362001a6366be32fea2
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-02 13:25:44 +0530

    Update regex.py

--- Git Python/Python core/regex.py
+++ Git Python/Python core/regex.py
@@ -17,63 +17,63 @@
 #re.U -->Interprets letters according to the Unicode character set. This flag affects the behavior of \w, \W, \b, \B.
 #re.X -->Permits "cuter" regular expression syntax. It ignores whitespace (except inside a set [] or when escaped by a backslash) and treats unescaped # as a comment marker.
 
-^-->Matches beginning of line.
-$-->?Matches end of line.
-.-->?Matches any single character except newline. Using m option allows it to match newline as well.
-[...]-->?Matches any single character in brackets.
-[^...]-->?Matches any single character not in brackets
-re*-->Matches 0 or more occurrences of preceding expression.
-re+-->Matches 1 or more occurrence of preceding expression.
-re?-->Matches 0 or 1 occurrence of preceding expression.
-re{ n}-->?Matches exactly n number of occurrences of preceding expression.
-re{ n,}-->Matches n or more occurrences of preceding expression.
-re{ n, m} -->Matches at least n and at most m occurrences of preceding expression.
-a| b-->Matches either a or b.
-(re)-->Groups regular expressions and remembers matched text.
-(?imx)-->Temporarily toggles on i, m, or x options within a regular expression. If in parentheses, only that area is affected.
-(?-imx)-->Temporarily toggles off i, m, or x options within a regular expression. If in parentheses, only that area is affected.
-(?: re)-->Groups regular expressions without remembering matched text.
-(?imx: re)-->Temporarily toggles on i, m, or x options within parentheses.
-(?-imx: re)-->Temporarily toggles off i, m, or x options within parentheses.
-(?#...)-->Comment.
-(?= re)-->Specifies position using a pattern. Doesn't have a range.
-(?! re)-->Specifies position using pattern negation. Doesn't have a range.
-(?> re)-->Matches independent pattern without backtracking.
-\w-->Matches word characters.
-\W-->Matches nonword characters.
-\s-->Matches whitespace. Equivalent to [\t\n\r\f].
-\S-->Matches nonwhitespace.
-\d-->Matches digits. Equivalent to [0-9].
-\D-->Matches nondigits.
-\A-->Matches beginning of string.
-\Z-->Matches end of string. If a newline exists, it matches just before newline.
-\z-->Matches end of string.
-\G-->Matches point where last match finished.
-\b-->Matches word boundaries when outside brackets. Matches backspace (0x08) when inside brackets.
-\B-->Matches nonword boundaries.
-\n, \t, etc.-->Matches newlines, carriage returns, tabs, etc.
-\1...\9-->Matches nth grouped subexpression.
-\10-->Matches nth grouped subexpression if it matched already. Otherwise refers to the octal representation of a character code.
+#^-->Matches beginning of line.
+#$-->?Matches end of line.
+#.-->?Matches any single character except newline. Using m option allows it to match newline as well.
+#[...]-->?Matches any single character in brackets.
+#[^...]-->?Matches any single character not in brackets
+#re*-->Matches 0 or more occurrences of preceding expression.
+#re+-->Matches 1 or more occurrence of preceding expression.
+#re?-->Matches 0 or 1 occurrence of preceding expression.
+#re{ n}-->?Matches exactly n number of occurrences of preceding expression.
+#re{ n,}-->Matches n or more occurrences of preceding expression.
+#re{ n, m} -->Matches at least n and at most m occurrences of preceding expression.
+#a| b-->Matches either a or b.
+#(re)-->Groups regular expressions and remembers matched text.
+#(?imx)-->Temporarily toggles on i, m, or x options within a regular expression. If in parentheses, only that area is affected.
+#(?-imx)-->Temporarily toggles off i, m, or x options within a regular expression. If in parentheses, only that area is affected.
+#(?: re)-->Groups regular expressions without remembering matched text.
+#(?imx: re)-->Temporarily toggles on i, m, or x options within parentheses.
+#(?-imx: re)-->Temporarily toggles off i, m, or x options within parentheses.
+#(?#...)-->Comment.
+#(?= re)-->Specifies position using a pattern. Doesn't have a range.
+#(?! re)-->Specifies position using pattern negation. Doesn't have a range.
+#(?> re)-->Matches independent pattern without backtracking.
+#\w-->Matches word characters.
+#\W-->Matches nonword characters.
+#\s-->Matches whitespace. Equivalent to [\t\n\r\f].
+#\S-->Matches nonwhitespace.
+#\d-->Matches digits. Equivalent to [0-9].
+#\D-->Matches nondigits.
+#\A-->Matches beginning of string.
+#\Z-->Matches end of string. If a newline exists, it matches just before newline.
+#\z-->Matches end of string.
+#\G-->Matches point where last match finished.
+#\b-->Matches word boundaries when outside brackets. Matches backspace (0x08) when inside brackets.
+#\B-->Matches nonword boundaries.
+#\n, \t, etc.-->Matches newlines, carriage returns, tabs, etc.
+#\1...\9-->Matches nth grouped subexpression.
+#\10-->Matches nth grouped subexpression if it matched already. Otherwise refers to the octal representation of a character code.
 
 #character class
-[Pp]ython-->Match "Python" or "python"
-rub[ye]-->Match "ruby" or "rube"
-[aeiou]-->Match any one lowercase vowel
-[0-9]-->Match any digit; same as [0123456789]
-[a-z]-->Match any lowercase ASCII letter
-[A-Z]-->Match any uppercase ASCII letter
-[a-zA-Z0-9]-->Match any of the above
-[^aeiou]-->Match anything other than a lowercase vowel
-[^0-9]-->Match anything other than a digit
+#[Pp]ython-->Match "Python" or "python"
+#rub[ye]-->Match "ruby" or "rube"
+#[aeiou]-->Match any one lowercase vowel
+#[0-9]-->Match any digit; same as [0123456789]
+#[a-z]-->Match any lowercase ASCII letter
+#[A-Z]-->Match any uppercase ASCII letter
+#[a-zA-Z0-9]-->Match any of the above
+#[^aeiou]-->Match anything other than a lowercase vowel
+#[^0-9]-->Match anything other than a digit
 
 
-.-->Match any character except newline
-\d-->Match a digit: [0-9]
-\D-->Match a nondigit: [^0-9]
-\s-->Match a whitespace character: [ \t\r\n\f]
-\S-->Match nonwhitespace: [^ \t\r\n\f]
-\w-->Match a single word character: [A-Za-z0-9_]
-\W-->Match a nonword character: [^A-Za-z0-9_]
+#.-->Match any character except newline
+#\d-->Match a digit: [0-9]
+#\D-->Match a nondigit: [^0-9]
+#\s-->Match a whitespace character: [ \t\r\n\f]
+#\S-->Match nonwhitespace: [^ \t\r\n\f]
+#\w-->Match a single word character: [A-Za-z0-9_]
+#\W-->Match a nonword character: [^A-Za-z0-9_]
 
 
 ===============================
