bc3e457b54d8bc977f73356784d85ce009edbc03
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-04 16:22:55 +0530

    Update datatypes_tuple.py

--- Git Python/Python core/datatypes_tuple.py
+++ Git Python/Python core/datatypes_tuple.py
@@ -1,4 +1,152 @@
 #datatypes_tuple.py
+
+#In Python programming, a tuple is similar to a list.
+#The difference between the two is that we cannot change the elements of a tuple once it is assigned whereas in a list, elements can be changed.
+
+#Advantages of Tuple over List:
+#1.Since, tuples are quite similiar to lists, both of them are used in similar situations as well.
+#2.However, there are certain advantages of implementing a tuple over a list. Below listed are some of the main advantages:
+#3.We generally use tuple for heterogeneous (different) datatypes and list for homogeneous (similar) datatypes.
+#4.Since tuple are immutable, iterating through tuple is faster than with list. So there is a slight performance boost.
+#5.Tuples that contain immutable elements can be used as key for a dictionary. With list, this is not possible.
+#6.If you have data that doesn't change, implementing it as tuple will guarantee that it remains write-protected.
+
+#Creating a Tuple
+#1.A tuple is created by placing all the items (elements) inside a parentheses (), separated by comma.The parentheses are optional but is a good practice to write it.
+#2.A tuple can have any number of items and they may be of different types (integer, float, list, string etc.).
+
+#Creating a tuple with one element is a bit tricky.
+#Having one element within parentheses is not enough. We will need a trailing comma to indicate that it is in fact a tuple.
+my_tuple = ("hello")
+print(type(my_tuple))
+my_tuple = ("hello",)  
+print(type(my_tuple))
+my_tuple = "hello",
+print(type(my_tuple))
+#<class 'str'>
+#<class 'tuple'>
+#<class 'tuple'>
+
+
+#Accessing Elements in a Tuple -->
+#There are various ways in which we can access the elements of a tuple.
+#1. Indexing :We can use the index operator [] to access an item in a tuple where the index starts from 0.
+#So, a tuple having 6 elements will have index from 0 to 5. Trying to access an element other that (6, 7,...) will raise an IndexError.
+#The index must be an integer, so we cannot use float or other types. This will result into TypeError.
+my_tuple = ('p','e','r','m','i','t')
+print(my_tuple[0])
+print(my_tuple[5])
+#print(my_tuple[6]) --> error
+#my_tuple[2.0] -->error
+n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(n_tuple[0][3])
+print(n_tuple[1][1])
+#p
+#t
+#s
+#4
+
+#2. Negative Indexing
+#Python allows negative indexing for its sequences.
+#The index of -1 refers to the last item, -2 to the second last item and so on.
+
+3. Slicing
+#We can access a range of items in a tuple by using the slicing operator - colon ":".
+
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+print(my_tuple[1:4])
+print(my_tuple[:-7])
+print(my_tuple[7:])
+print(my_tuple[:])
+#('r', 'o', 'g')
+#('p', 'r')
+#('i', 'z')
+#('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+
+#4.Changing a Tuple
+#Unlike lists, tuples are immutable.
+#This means that elements of a tuple cannot be changed once it has been assigned. But, if the element is itself a mutable datatype like list, its nested items can be changed.
+#We can also assign a tuple to different values (reassignment).
+my_tuple = (4, 2, 3, [6, 5])
+#my_tuple[1] = 9 error -->
+# Output: (4, 2, 3, [9, 5])
+my_tuple[3][0] = 9
+print(my_tuple)
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+print(my_tuple)
+#(4, 2, 3, [9, 5])
+#('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+
+#We can use + operator to combine two tuples. This is also called concatenation.
+#We can also repeat the elements in a tuple for a given number of times using the * operator.
+#Both + and * operations result into a new tuple.
+
+print((1, 2, 3) + (4, 5, 6))
+print(("Repeat",) * 3)
+#(1, 2, 3, 4, 5, 6)
+#('Repeat', 'Repeat', 'Repeat')
+
+#5.Deleting a Tuple
+#As discussed above, we cannot change the elements in a tuple. That also means we cannot delete or remove items from a tuple.
+#But deleting a tuple entirely is possible using the keyword del.
+my_tuple = ('p','r','o','g','r','a','m','i','z')
+#del my_tuple[3] -->error
+del my_tuple
+print(my_tuple) --> No tuple exist as error
+
+#Python Tuple Methods -->
+#Methods that add items or remove items are not available with tuple. Only the following two methods are available.
+#Method		Description
+#count(x)		Return the number of items that is equal to x
+#index(x)		Return index of first item that is equal to x
+
+my_tuple = ('a','p','p','l','e',)
+print(my_tuple.count('p'))
+print(my_tuple.index('l'))
+#2
+#3
+
+#Tuple Membership Test
+#We can test if an item exists in a tuple or not, using the keyword in.
+my_tuple = ('a','p','p','l','e',)
+print('a' in my_tuple)
+print('b' in my_tuple)
+print('g' not in my_tuple)
+#True
+#False
+#True
+
+#Iterating Through a Tuple
+#Using a for loop we can iterate though each item in a tuple.
+for name in ('John','Kate'):
+     print("Hello",name) 
+#Hello John
+#Hello Kate
+
+#Built-in Functions with Tuple
+#Built-in functions like all(), any(), enumerate(), len(), max(), min(), sorted(), tuple() etc. are commonly used with tuple to perform different tasks.
+Function		Description
+all()		Return True if all elements of the tuple are true (or if the tuple is empty).
+any()		Return True if any element of the tuple is true. If the tuple is empty, return False.
+enumerate()		Return an enumerate object. It contains the index and value of all the items of tuple as pairs.
+len()		Return the length (the number of items) in the tuple.
+max()		Return the largest item in the tuple.
+min()		Return the smallest item in the tuple
+sorted()		Take elements in the tuple and return a new sorted list (does not sort the tuple itself).
+sum()		Retrun the sum of all elements in the tuple.
+tuple()		Convert an iterable (list, string, set, dictionary) to a tuple.
+
+
+
+
+
+
+
+
+
+
+
+
 ====================================================
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
