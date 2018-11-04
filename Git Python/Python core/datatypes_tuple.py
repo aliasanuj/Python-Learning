d28f6dd4a2569cf0df6c49c248c17b721098b451
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-04 16:24:34 +0530

    Update datatypes_tuple.py

--- Git Python/Python core/datatypes_tuple.py
+++ Git Python/Python core/datatypes_tuple.py
@@ -128,7 +128,7 @@ for name in ('John','Kate'):
 Function		Description
 all()		Return True if all elements of the tuple are true (or if the tuple is empty).
 any()		Return True if any element of the tuple is true. If the tuple is empty, return False.
-enumerate()		Return an enumerate object. It contains the index and value of all the items of tuple as pairs.
+enumerate()	Return an enumerate object. It contains the index and value of all the items of tuple as pairs.
 len()		Return the length (the number of items) in the tuple.
 max()		Return the largest item in the tuple.
 min()		Return the smallest item in the tuple
