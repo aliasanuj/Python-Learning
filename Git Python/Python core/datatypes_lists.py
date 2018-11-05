2b147f8c04fa3e7f85f6f0824d7d702024897e05
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-05 17:49:43 +0530

    Update datatypes_lists.py

--- Git Python/Python core/datatypes_lists.py
+++ Git Python/Python core/datatypes_lists.py
@@ -506,50 +506,33 @@ for fruit in ['apple','banana','mango']:
 List = [] 
 print("Intial blank List: ") 
 print(List) 
-# Creating a List with  
-# the use of a String 
 List = ['GeeksForGeeks'] 
 print("\nList with the use of String: ") 
 print(List) 
-# Creating a List with 
-# the use of multiple values 
 List = ["Geeks", "For", "Geeks"] 
 print("\nList containing multiple values: ") 
 print(List[0])  
 print(List[2]) 
-# Creating a Multi-Dimensional List 
-# (By Nesting a list inside a List) 
 List = [['Geeks', 'For'] , ['Geeks']] 
 print("\nMulti-Dimensional List: ") 
 print(List) 
-# Creating a List with  
-# the use of Numbers 
-# (Having duplicate values) 
 List = [1, 2, 4, 4, 3, 3, 3, 6, 5] 
 print("\nList with the use of Numbers: ") 
 print(List) 
-# Creating a List with  
-# mixed type of values 
-# (Having numbers and strings) 
 List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
 print("\nList with the use of Mixed Values: ") 
 print(List) 
 #Intial blank List: 
 #[]
-
 #List with the use of String: 
 #['GeeksForGeeks']
-
 #List containing multiple values: 
 #Geeks
 #Geeks
-
 #Multi-Dimensional List: 
 #[['Geeks', 'For'], ['Geeks']]
-
 #List with the use of Numbers: 
 #[1, 2, 4, 4, 3, 3, 3, 6, 5]
-
 #List with the use of Mixed Values: 
 #[1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
 ==============================================
