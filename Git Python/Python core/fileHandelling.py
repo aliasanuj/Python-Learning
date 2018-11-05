595711fcc552868cedc898ca1844d29567003ee4
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-06 02:33:56 +0530

    Update fileHandelling.py

--- Git Python/Python core/fileHandelling.py
+++ Git Python/Python core/fileHandelling.py
@@ -1,6 +1,27 @@
 #File handelling 
 =======================
-
+#r-->Opens a file for reading only. The file pointer is placed at the beginning of the file. This is the default mode.
+#rb-->Opens a file for reading only in binary format. The file pointer is placed at the beginning of the file. This is the default mode.
+#r+-->Opens a file for both reading and writing. The file pointer placed at the beginning of the file.
+#rb+-->Opens a file for both reading and writing in binary format. The file pointer placed at the beginning of the file.
+#w-->Opens a file for writing only. Overwrites the file if the file exists. If the file does not exist, creates a new file for writing.
+#wb-->Opens a file for writing only in binary format. Overwrites the file if the file exists. If the file does not exist, creates a new file for writing.
+#w+-->Opens a file for both writing and reading. Overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
+#wb+-->Opens a file for both writing and reading in binary format. Overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
+#a-->Opens a file for appending. The file pointer is at the end of the file if the file exists. That is, the file is in the append mode. If the file does not exist, it creates a new file for writing.
+#ab-->?Opens a file for appending in binary format. The file pointer is at the end of the file if the file exists. That is, the file is in the append mode. If the file does not exist, it creates a new file for writing.
+#a+-->Opens a file for both appending and reading. The file pointer is at the end of the file if the file exists. The file opens in the append mode. If the file does not exist, it creates a new file for reading and writing.
+#ab+-->Opens a file for both appending and reading in binary format. The file pointer is at the end of the file if the file exists. The file opens in the append mode. If the file does not exist, it creates a new file for reading and writing.
+============================
+Mode	Description
+'r'	Open a file for reading. (default)
+'w'	Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+'x'	Open a file for exclusive creation. If the file already exists, the operation fails.
+'a'	Open for appending at the end of the file without truncating it. Creates a new file if it does not exist.
+'t'	Open in text mode. (default)
+'b'	Open in binary mode.
+'+'	Open a file for updating (reading and writing)
+============================
 #File handling is an important part of any web application.
 #Python has several functions for creating, reading, updating, and deleting files.
 
