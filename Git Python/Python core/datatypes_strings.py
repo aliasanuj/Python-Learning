9fd133994905f4ccc84e789c6962587636b1a9f3
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-05 19:30:47 +0530

    Update datatypes_strings.py

--- Git Python/Python core/datatypes_strings.py
+++ Git Python/Python core/datatypes_strings.py
@@ -1,5 +1,167 @@
 #datatypes_strings.py
 ===============================
+#What is String in Python --> 
+#A string is a sequence of characters.
+#A character is simply a symbol. For example, the English language has 26 characters.
+#Computers do not deal with characters, they deal with numbers (binary).
+#Even though you may see characters on your screen, internally it is stored and manipulated as a combination of 0's and 1's.
+#This conversion of character to a number is called encoding, and the reverse process is decoding. ASCII and Unicode are some of the popular encoding used.
+#In Python, string is a sequence of Unicode character.
+#Unicode was introduced to include every character in all languages and bring uniformity in encoding. You can learn more about Unicode from here.
+
+#How to create a string in Python -->
+#Strings can be created by enclosing characters inside a single quote or double quotes.
+#Even triple quotes can be used in Python but generally used to represent multiline strings and docstrings.
+
+# all of the following are equivalent
+my_string = 'Hello'
+print(my_string)
+my_string = "Hello"
+print(my_string)
+my_string = '''Hello'''
+print(my_string)
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
+#When you run the program, the output will be:
+Hello
+Hello
+Hello
+Hello, welcome to
+           the world of Python
+
+#How to access characters in a string -->
+#We can access individual characters using indexing and a range of characters using slicing.
+#Index starts from 0. Trying to access a character out of index range will raise an IndexError.
+#The index must be an integer. We can't use float or other types, this will result into TypeError.
+#Python allows negative indexing for its sequences.
+#The index of -1 refers to the last item, -2 to the second last item and so on.
+#We can access a range of items in a string by using the slicing operator (colon).
+
+str = 'programiz'
+print('str = ', str)
+#first character
+print('str[0] = ', str[0])
+#last character
+print('str[-1] = ', str[-1])
+#slicing 2nd to 5th character
+print('str[1:5] = ', str[1:5])
+#slicing 6th to 2nd last character
+print('str[5:-2] = ', str[5:-2])
+#str =  programiz
+#str[0] =  p
+#str[-1] =  z
+#str[1:5] =  rogr
+#str[5:-2] =  am
+
+If we try to access index out of the range or use decimal number, we will get errors.
+# index must be in range
+>>> my_string[15]  
+IndexError: string index out of range
+# index must be an integer
+>>> my_string[1.5] 
+TypeError: string indices must be integers
+
+#How to change or delete a string -->
+#Strings are immutable.
+#This means that elements of a string cannot be changed once it has been assigned.
+#We can simply reassign different strings to the same name.
+
+#Concatenation of Two or More Strings -->
+#Joining of two or more strings into a single one is called concatenation.
+#The + operator does this in Python. Simply writing two string literals together also concatenates them.
+#The * operator can be used to repeat the string for a given number of times.
+
+str1 = 'Hello'
+str2 ='World!'
+# using +
+print('str1 + str2 = ', str1 + str2)
+# using *
+print('str1 * 3 =', str1 * 3)
+#str1 + str2 =  HelloWorld!
+#str1 * 3 = HelloHelloHello
+
+#Iterating Through String -->
+#Using for loop we can iterate through a string. Here is an example to count the number of 'l' in a string.
+count = 0
+for letter in 'Hello World':
+    if(letter == 'l'):
+        count += 1
+print(count,'letters found')
+#3 letters found
+
+#String Membership Test
+#We can test if a sub string exists within a string or not, using the keyword in.
+>>> 'a' in 'program'
+True
+>>> 'at' not in 'battle'
+False
+
+#Built-in functions to Work with Python -->
+#Various built-in functions that work with sequence, works with string as well.
+#Some of the commonly used ones are enumerate() and len(). The enumerate() function returns an enumerate object.
+#It contains the index and value of all the items in the string as pairs. This can be useful for iteration.
+#Similarly, len() returns the length (number of characters) of the string.
+
+str = 'cold'
+# enumerate()
+list_enumerate = list(enumerate(str))
+print('list(enumerate(str) = ', list_enumerate)
+#character count
+print('len(str) = ', len(str))
+for i in enumerate(str):
+    print(i)
+
+#list(enumerate(str) =  [(0, 'c'), (1, 'o'), (2, 'l'), (3, 'd')]
+#len(str) =  4
+#(0, 'c')
+#(1, 'o')
+#(2, 'l')
+#(3, 'd')
+
+#Escape Sequence -->
+#If we want to print a text like -He said, "What's there?"- we can neither use single quote or double quotes.
+#This will result into SyntaxError as the text itself contains both single and double quotes.
+>>> print("He said, "What's there?"")
+SyntaxError: invalid syntax
+>>> print('He said, "What's there?"')
+SyntaxError: invalid syntax
+
+#One way to get around this problem is to use triple quotes. Alternatively, we can use escape sequences.
+#An escape sequence starts with a backslash and is interpreted differently.
+#If we use single quote to represent a string, all the single quotes inside the string must be escaped.
+#Similar is the case with double quotes. 
+
+# using triple quotes
+print('''He said, "What's there?"''')
+# escaping single quotes
+print('He said, "What\'s there?"')
+# escaping double quotes
+print("He said, \"What's there?\"")
+#He said, "What's there?"
+#He said, "What's there?"
+#He said, "What's there?"
+
+#Here is a list of all the escape sequence supported by Python.
+#Escape Sequence in Python
+#Escape Sequence	Description
+\newline		Backslash and newline ignored
+\\		Backslash
+\'		Single quote
+\"		Double quote
+\a		ASCII Bell
+\b		ASCII Backspace
+\f		ASCII Formfeed
+\n		ASCII Linefeed
+\r		ASCII Carriage Return
+\t		ASCII Horizontal Tab
+\v		ASCII Vertical Tab
+\ooo		Character with octal value ooo
+\xHH		Character with hexadecimal value HH
+
+
+===============================
 a = "Hello, World!"
 print(a[1])
 #e
