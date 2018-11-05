cfcaee0dcd7157b72d87b63ee70271eff2f4d8a5
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-05 12:15:50 +0530

    Update datatypes_lists.py

--- Git Python/Python core/datatypes_lists.py
+++ Git Python/Python core/datatypes_lists.py
@@ -156,17 +156,17 @@ print(my_list)
 #They are accessed as list.method(). Some of the methods have already been used above.
 
 #Python List Methods
-append() - Add an element to the end of the list
-extend() - Add all elements of a list to the another list
-insert() - Insert an item at the defined index
-remove() - Removes an item from the list
-pop() - Removes and returns an element at the given index
-clear() - Removes all items from the list
-index() - Returns the index of the first matched item
-count() - Returns the count of number of items passed as an argument
-sort() - Sort items in a list in ascending order
-reverse() - Reverse the order of items in the list
-copy() - Returns a shallow copy of the list
+append()	Add an element to the end of the list
+extend()	Add all elements of a list to the another list
+insert()	Insert an item at the defined index
+remove()	Removes an item from the list
+pop()		Removes and returns an element at the given index
+clear()		Removes all items from the list
+index()		Returns the index of the first matched item
+count()		Returns the count of number of items passed as an argument
+sort()		Sort items in a list in ascending order
+reverse()	Reverse the order of items in the list
+copy()		Returns a shallow copy of the list
 
 #Function	Description
 all()	Return True if all elements of the list are true (or if the list is empty).
