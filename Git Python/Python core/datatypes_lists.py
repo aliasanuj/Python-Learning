e264b61404144844c245816953cebe12ac451cc0
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-05 12:17:28 +0530

    Update datatypes_lists.py

--- Git Python/Python core/datatypes_lists.py
+++ Git Python/Python core/datatypes_lists.py
@@ -169,15 +169,15 @@ reverse()	Reverse the order of items in the list
 copy()		Returns a shallow copy of the list
 
 #Function	Description
-all()	Return True if all elements of the list are true (or if the list is empty).
-any()	Return True if any element of the list is true. If the list is empty, return False.
+all()		Return True if all elements of the list are true (or if the list is empty).
+any()		Return True if any element of the list is true. If the list is empty, return False.
 enumerate()	Return an enumerate object. It contains the index and value of all the items of list as a tuple.
-len()	Return the length (the number of items) in the list.
-list()	Convert an iterable (tuple, string, set, dictionary) to a list.
-max()	Return the largest item in the list.
-min()	Return the smallest item in the list
-sorted()	Return a new sorted list (does not sort the list itself).
-sum()	Return the sum of all elements in the list.
+len()		Return the length (the number of items) in the list.
+list()		Convert an iterable (tuple, string, set, dictionary) to a list.
+max()		Return the largest item in the list.
+min()		Return the smallest item in the list
+sorted()		Return a new sorted list (does not sort the list itself).
+sum()		Return the sum of all elements in the list.
 
 
 my_list = [3, 8, 1, 6, 0, 8, 4]
