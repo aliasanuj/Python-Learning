25283a2b75037b2639c5d0a9bd7fbd4f6772df8c
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-05 22:10:02 +0530

    Update datatypes_strings.py

--- Git Python/Python core/datatypes_strings.py
+++ Git Python/Python core/datatypes_strings.py
@@ -256,11 +256,11 @@ replace()			returns a copy of the string where all occurrences of a substring is
 ======================================
 BUILT-IN FUNCTION	DESCRIPTION
 =======================================
-string.Isdecimal		Returns true if all characters in a string are decimal
+string.Isdecimal	Returns true if all characters in a string are decimal
 String.Isalnum		Returns true if all the characters in a given string are alphanumeric.
 string.Istitle		Returns True if the string is a titlecased string
-String.partition		splits the string at the first occurrence of the separator and returns a tuple.
-String.Isidentifier		Check whether a string is a valid identifier or not.
+String.partition	splits the string at the first occurrence of the separator and returns a tuple.
+String.Isidentifier	Check whether a string is a valid identifier or not.
 String.len		Returns the length of the string.
 String.rindex		Returns the highest index of the substring inside the string if substring is found.
 String.Max		Returns the highest alphabetical character in a string.
