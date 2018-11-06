ffa2ae25cc7425a950564e0fc096abffd8942fe9
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-06 12:03:14 +0530

    Update exceptation_handelling.py

--- Git Python/Python core/exceptation_handelling.py
+++ Git Python/Python core/exceptation_handelling.py
@@ -168,6 +168,108 @@ ValueError		Raised when a function gets argument of correct type but improper va
 IOError			Raised when an input/ output operation fails
 RuntimeError		Raised when a generated error does not fall into any category
 =======================================================
+#Exceptions versus Syntax Errors :
+#Syntax errors occur when the parser detects an incorrect statement. Observe the following example:
+
+>>> print( 0 / 0 ))
+  File "<stdin>", line 1
+    print( 0 / 0 ))
+                  ^
+#SyntaxError: invalid syntax
+#The arrow indicates where the parser ran into the syntax error. 
+#In this example, there was one bracket too many. Remove it and run your code again:
+
+>>> print( 0 / 0)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ZeroDivisionError: integer division or modulo by zero
+
+#This time, you ran into an exception error. This type of error occurs whenever 
+#syntactically correct Python code results in an error. 
+#The last line of the message indicated what type of exception error you ran into.
+#Instead of showing the message exception error, Python details what type 
+#of exception error was encountered. In this case, it was a ZeroDivisionError.
+#Python comes with various built-in exceptions as well as the possibility to create self-defined exceptions.
+
+
+#Raising an Exception
+#We can use raise to throw an exception if a condition occurs.
+#The statement can be complemented with a custom exception.
+#If you want to throw an error when a certain condition occurs using raise, 
+#you could go about it like this:
+
+x = 10
+if x > 5:
+    raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
+
+	When you run this code, the output will be the following:
+
+Traceback (most recent call last):
+  File "<input>", line 4, in <module>
+Exception: x should not exceed 5. The value of x was: 10
+#The program comes to a halt and displays our exception to screen, 
+#offering clues about what went wrong.
+
+
+#The AssertionError Exception
+#Instead of waiting for a program to crash midway, you can also start by making an assertion in Python.
+# We assert that a certain condition is met. If this condition turns out to be True, then that
+# is excellent! The program can continue. If the condition turns out to be False, you can have the program 
+#throw an AssertionError exception.
+
+#Python assert statement :
+#Have a look at the following example, where it is asserted that the code will be executed on a Linux system:
+
+import sys
+assert ('linux' in sys.platform), "This code runs on Linux only."
+#If you run this code on a Linux machine, the assertion passes.
+#If you were to run this code on a Windows machine, the outcome of the
+#assertion would be False and the result would be the following:
+
+Traceback (most recent call last):
+  File "<input>", line 2, in <module>
+AssertionError: This code runs on Linux only.
+In this example, throwing an AssertionError exception is the last thing that the program will do. The program will come to halt and will not continue. What if that is not what you want?
+
+#The try and except Block: Handling Exceptions
+#The try and except block in Python is used to catch and handle exceptions. 
+#Python executes code following the try statement as a “normal” part of the program. 
+#The code that follows the except statement is the program’s response to any exceptions 
+#in the preceding try clause.
+
+#As you saw earlier, when syntactically correct code runs into an error,
+#Python will throw an exception error. This exception error will crash the 
+#program if it is unhandled. The except clause determines how your program responds to exceptions.
+
+#The following function can help you understand the try and except block:
+
+def linux_interaction():
+    assert ('linux' in sys.platform), "Function can only run on Linux systems."
+    print('Doing something.')
+#The linux_interaction() can only run on a Linux system. 
+#The assert in this function will throw an AssertionError exception 
+#if you call it on an operating system other then Linux.
+#You can give the function a try using the following code:
+
+try:
+    linux_interaction()
+except:
+    pass
+
+#The way you handled the error here is by handing out a pass. 
+#If you were to run this code on a Windows machine, you would get the following output:
+
+#Summing Up :
+#1.After seeing the difference between syntax errors and exceptions, you learned about 
+#various ways to raise, catch, and handle exceptions in Python. In this article, you saw the following options:
+#2.raise allows you to throw an exception at any time.
+#3.assert enables you to verify if a certain condition is met and throw an exception if it isn’t.
+#4.In the try clause, all statements are executed until an exception is encountered.
+#5.except is used to catch and handle the exception(s) that are encountered in the try clause.
+#6.else lets you code sections that should run only when no exceptions are encountered in the try clause.
+#7.finally enables you to execute sections of code that should always run, 
+#with or without any previously encountered exceptions.
+
 ========================================================
 try:
     print (1/0)
