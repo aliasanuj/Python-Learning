781519bae41ffeeea947e6a7259de031332d3221
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-08 18:14:41 +0530

    Update MultiThreading.py

--- Git Python/Python core/MultiThreading.py
+++ Git Python/Python core/MultiThreading.py
@@ -24,8 +24,8 @@ def print_square(num):
     print("Square: {}".format(num * num)) 
   
 if __name__ == "__main__": 
-    t1 = threading.Thread(target=print_square, args=(10,)) 
-    t2 = threading.Thread(target=print_cube, args=(10,)) 
+    t1 = threading.Thread(target=print_square, args=(10,)) #args=(10,) must be tuple
+    t2 = threading.Thread(target=print_cube, args=(10,)) #args=(10,) must be tuple
     t1.start() 
     t2.start() 
     t1.join() 
