b78b06b85b589e41ede2fa29cbb8a4cb81e4a7ec
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-09 01:37:34 +0530

    Update datatypes_dict.py

--- Git Python/Python core/datatypes_dict.py
+++ Git Python/Python core/datatypes_dict.py
@@ -280,6 +280,17 @@ my_dict = {'name':'Jack', 'age': 26}
 print(my_dict['name'])
 # Output: 26
 print(my_dict.get('age'))
+print(my_dict.get("anuj"))
+
+#Jack
+#26
+#None
+==============================================
+my_dict = {'name':'Jack', 'age': 26}
+# Output: Jack
+print(my_dict['name'])
+# Output: 26
+print(my_dict.get('age'))
 # Trying to access keys which doesn't exist throws error
 # my_dict.get('address')
 # my_dict['address']
