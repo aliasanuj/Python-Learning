82bd2ef7d5361ac9ac445a8ecd2e604831f3e915
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-08 23:14:35 +0530

    Update counters.py

--- Git Python/Python core/counters.py
+++ Git Python/Python core/counters.py
@@ -28,7 +28,7 @@ print(result1)
 string2 = ["anuj","aman","anuj"]
 result2 = Counter(string2)
 print(result2)
-#Counter({'u': 2, 'a': 2, 'k': 1, 'm': 1, 'r': 1, 'e': 1, ' ': 1, #'n': 1, 'j': 1})
+#Counter({'u': 2, 'a': 2, 'k': 1, 'm': 1, 'r': 1, 'e': 1, ' ': 1, 'n': 1, 'j': 1})
 #Counter({'anuj': 2, 'aman': 1})
 =======================
 from collections import Counter 
