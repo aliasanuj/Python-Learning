f18ab5e3fdb2a043e582e127b61e581e3f95abdc
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-12 00:07:45 +0530

    Update decorator.py

--- Git Python/Python core/decorator.py
+++ Git Python/Python core/decorator.py
@@ -1,6 +1,56 @@
 ==========================
 #decorator================
 ==========================
+#In Python, we can define a function inside another function.
+#In Python, a function can be passed as parameter to another function (a function can also return another function).
+#Python has an interesting feature called decorators to add functionality to an existing code.
+#This is also called metaprogramming as a part of the program tries to modify another part of the program at compile time.
+#When you run the code, both functions first and second gives same output. Here, the names first and second refer to the same function object.
+#Now things start getting weirder.
+#Functions can be passed as arguments to another function.
+#If you have used functions like map, filter and reduce in Python, then you already know about this.
+
+Such function that take other functions as arguments are also called higher order functions. Here is an example of such a function.
+def first(msg):
+    print(msg)
+
+first("Hello")
+second = first
+second("Hello")
+
+#Hello
+#Hello
+=============================
+def add1(a):
+  return 1+a
+
+def add2(a):
+  return 2+a
+
+def abc(func,a):
+  result = func(a)
+  print(result)
+
+abc(add1,5)
+abc(add2,6)
+#6
+#8
+==============================
+def make_pretty(func):
+    def inner():
+        print("I got decorated")
+        func()
+    return inner
+
+def ordinary():
+    print("I am ordinary")
+
+pretty = make_pretty(ordinary)
+pretty()
+
+#I got decorated
+#I am ordinary
+===================================
 def outer(abc):
     def inner():
         print("inner function")
