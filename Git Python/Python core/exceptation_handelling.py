bac86de5460c6ed729fb005ee84907f18b00c8da
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-12 01:37:53 +0530

    Update exceptation_handelling.py

--- Git Python/Python core/exceptation_handelling.py
+++ Git Python/Python core/exceptation_handelling.py
@@ -406,6 +406,31 @@ while True:
 print("Congratulations! You guessed it correctly.")
 #Enter a number: 45
 #This value is too large, try again!
+====================================================
+class Error(Exception):
+  pass
+class ValueTooSmallError(Error):
+  pass
+class ValueTooLargeError(Error):
+  pass
+number = 10
+while True:
+   try:
+       i_num = int(input("Enter a number: "))
+       if i_num < number:
+           raise ValueTooSmallError
+       elif i_num > number:
+           raise ValueTooLargeError
+       break
+   except ValueTooSmallError:
+       print("This value is too small, try again!")
+       print()
+   except ValueTooLargeError:
+       print("This value is too large, try again!")
+       print()
+print("Congratulations! You guessed it correctly.")
+
+
 =======================================================
 class Error(Exception):
    """Base class for other exceptions"""
