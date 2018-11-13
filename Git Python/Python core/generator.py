0b04f66dfe8ca73a5aaefac715edbf6ab0e1c320
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-13 13:03:58 +0530

    Update generator.py

--- Git Python/Python core/generator.py
+++ Git Python/Python core/generator.py
@@ -1,4 +1,20 @@
 #generator :
+==============================
+#Generator 
+#a generator is a function that returns an object (iterator) which we can iterate over (one value at a time).
+#If a function contains at least one yield statement (it may contain other yield or return statements), 
+#it becomes a generator function. Both yield and return will return some value from a function.
+#The difference is that, while a return statement terminates a function entirely, yield 
+#statement pauses the function saving all its states and later continues from there on successive calls.
+
+#Generator function contains one or more yield statement.
+#When called, it returns an object (iterator) but does not start execution immediately.
+#Methods like __iter__() and __next__() are implemented automatically. So we can iterate through the items using next().
+#Once the function yields, the function is paused and the control is transferred to the caller.
+#Local variables and their states are remembered between successive calls.
+#Finally, when the function terminates, StopIteration is raised automatically on further calls.
+#It is also called lazy evaluation
+#
 =============================
 def my_gen():
     n = 1
