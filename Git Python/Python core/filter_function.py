97972ac743ba59e83b20c05b3d6865f8bb971a5c
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-13 14:09:07 +0530

    Update filter_function.py

--- Git Python/Python core/filter_function.py
+++ Git Python/Python core/filter_function.py
@@ -15,6 +15,12 @@ number_list = range(-5, 5)
 less_than_zero = list(filter(lambda x: x < 0, number_list))
 print(less_than_zero)
 #[-5, -4, -3, -2, -1]
+===========================
+number_list = range(-5, 5)
+less_than_zero = tuple(filter(lambda x: x < 0, number_list))
+print(less_than_zero)
+#(-5, -4, -3, -2, -1)
+
 ==========================
 def fun(variable): 
     letters = ['a', 'e', 'i', 'o', 'u'] 
