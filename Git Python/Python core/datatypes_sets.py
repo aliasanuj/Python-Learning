70a88487c40cae3fdd256b88635fd0be567f0710
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-13 12:20:10 +0530

    Update datatypes_sets.py

--- Git Python/Python core/datatypes_sets.py
+++ Git Python/Python core/datatypes_sets.py
@@ -74,9 +74,9 @@ Only instances of immutable types can be added to a Python set.
 
 ============================================
 Method	Description
-add()	  Adds an element to the set
-clear()	Removes all the elements from the set
-copy()	  Returns a copy of the set
+add()	      Adds an element to the set
+clear()	  Removes all the elements from the set
+copy()	    Returns a copy of the set
 difference()	Returns a set containing the difference between two or more sets
 difference_update()	Removes the items in this set that are also included in another, specified set
 discard()	Remove the specified item
@@ -85,7 +85,7 @@ intersection_update()	Removes the items in this set that are not present in othe
 isdisjoint()	Returns whether two sets have a intersection or not
 issubset()	Returns whether another set contains this set or not
 issuperset()	Returns whether this set contains another set or not
-pop()	  Removes an element from the set
+pop()	      Removes an element from the set
 remove()	Removes the specified element
 symmetric_difference()	Returns a set with the symmetric differences of two sets
 symmetric_difference_update()	inserts the symmetric differences from this set and another
