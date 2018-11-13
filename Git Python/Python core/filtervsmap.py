7676a5650e16760d29286b441bb8a5663cc90c46
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-13 15:33:07 +0530

    Update filtervsmap.py

--- Git Python/Python core/filtervsmap.py
+++ Git Python/Python core/filtervsmap.py
@@ -34,3 +34,7 @@ from functools import reduce
 product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
 # Output: 24
 
+from functools import reduce
+product = reduce((lambda x, y: x * y), (1, 2, 3, 4))
+print(product)
+#24
