4116cbf66be739af87949b885bf800184864ca21
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-16 21:09:32 +0530

    Create module.py

---
+++ Git Python/Python core/module.py
@@ -0,0 +1,18 @@
+#module 
+=====================
+
+=================================
+Modules used #https://docs.python.org/3/py-modindex.html
+=================================
+1. From collections import counter
+2. import threading 
+3. import copy 
+4. import mmap (file handelling in searching sttring)
+5. import random 
+6. import itertools 
+7. import operator (maths operation)
+8. from functools import partial (partial fractions)
+9. import functools import reduce(for reduce functions)
+10.import re 
+11. 
+12. 
