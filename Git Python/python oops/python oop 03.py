af23eac4f9240b75480097f00501f2ef53b7b4b5
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-20 11:59:55 +0530

    Update python oop 03.py

--- Git Python/python oops/python oop 03.py
+++ Git Python/python oops/python oop 03.py
@@ -543,6 +543,23 @@ class A:
     def test1(self):
         print(" test of A called ")
 class B(A):
+    def test1(self):
+        print(" test of B called ")
+class C(A):
+    def test(self):
+        print(" test of C called ")
+class D(C,B):
+    def test2(self):
+        print(" test of D called ")        
+obj=D()
+obj.test1()
+
+# test of B called 
+=================================
+class A:
+    def test1(self):
+        print(" test of A called ")
+class B(A):
     def test(self):
         print(" test of B called ")
 class C(A):
