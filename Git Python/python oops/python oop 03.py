c0da5226a3cb26429adaa1e6ffd6f44ed6ce3c53
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2018-11-20 12:08:36 +0530

    Update python oop 03.py

--- Git Python/python oops/python oop 03.py
+++ Git Python/python oops/python oop 03.py
@@ -596,6 +596,26 @@ obj.test()
 #methods of test() in three different classes is called.
 =================================
 class A:
+    def test(self):
+        print("test of A called")
+class B(A):
+    def test(self):
+        print("test of B called")
+        super().test()  
+class C(A):
+    def test1(self):
+        print("test of C called")
+        super().test()
+class D(B,C):
+    def test2(self):
+        print("test of D called")      
+obj=D()
+obj.test()
+
+#test of B called
+#test of A called
+=================================
+class A:
     def __str__(self):
         return '1'
 class B(A):
