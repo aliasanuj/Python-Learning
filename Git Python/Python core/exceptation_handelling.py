78ecc596d31e5392474def69de20c0fb9ea8516d
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-01-02 16:38:01 +0530

    Update exceptation_handelling.py

--- Git Python/Python core/exceptation_handelling.py
+++ Git Python/Python core/exceptation_handelling.py
@@ -1,18 +1,17 @@
 #exceptation_handelling.py
 ========================================================
-
-#An exception is an event, which occurs during the execution of a program that disrupts the normal flow
-#of the program's instructions.In general, when a Python script encounters a situation that it cannot
-#cope with, it raises an exception. An exception is a Python object that represents an error.
-#When a Python script raises an exception, it must either handle the exception immediately
-#otherwise it terminates and quits.
-
-#Handling an exception
-#If you have some suspicious code that may raise an exception,
-#you can defend your program by placing the suspicious code in a 
-#try: block. After the try: block, include an except: statement, 
-#followed by a block of code which handles the problem as elegantly as possible.
-
+An exception is an event, which occurs during the execution of a program that disrupts the normal flow
+of the program's instructions.In general, when a Python script encounters a situation that it cannot
+cope with, it raises an exception. An exception is a Python object that represents an error.
+When a Python script raises an exception, it must either handle the exception immediately
+otherwise it terminates and quits.
+==========================================================
+Handling an exception : 
+If you have some suspicious code that may raise an exception,
+you can defend your program by placing the suspicious code in a 
+try: block. After the try: block, include an except: statement, 
+followed by a block of code which handles the problem as elegantly as possible.
+==========================================================
 try:
    You do your operations here;
    ......................
@@ -23,16 +22,14 @@ except ExceptionII:
    ......................
 else:
    If there is no exception then execute this block. 
- 
-
-#A single try statement can have multiple except statements.
-#This is useful when the try block contains statements that may throw different types of exceptions.
-#You can also provide a generic except clause, which handles any exception.
-#After the except clause(s), you can include an else-clause. The code in
-#the else-block executes if the code in the try: block does not raise an exception.
-#The else-block is a good place for code that does not need the try: block's protection.
-
-
+ ==========================================================
+1. A single try statement can have multiple except statements.
+2. This is useful when the try block contains statements that may throw different types of exceptions.
+3. You can also provide a generic except clause, which handles any exception.
+4. After the except clause(s), you can include an else-clause. The code in
+5. The else-block executes if the code in the try: block does not raise an exception.
+6. The else-block is a good place for code that does not need the try: block's protection.
+==========================================================
 try:
    fh = open("testfile", "r")
    fh.write("This is my test file for exception handling!!")
@@ -40,10 +37,9 @@ except IOError:
    print "Error: can\'t find file or read data"
 else:
    print "Written content in the file successfully"
-   
-#The except Clause with No Exceptions
-#You can also use the except statement with no exceptions defined as follows −
-
+==========================================================   
+The except Clause with No Exceptions
+You can also use the except statement with no exceptions defined as follows −
 try:
    You do your operations here;
    ......................
@@ -52,18 +48,17 @@ except:
    ......................
 else:
    If there is no exception then execute this block. 
-   
-#This kind of a try-except statement catches all the exceptions that occur.
-#Using this kind of try-except statement is not considered a good programming
-#practice though, because it catches all exceptions but does not make the programmer
-#identify the root cause of the problem that may occur.
-
-#The try-finally Clause :
-#You can use a finally: block along with a try: block.
-#The finally block is a place to put any code that must execute, 
-#whether the try-block raised an exception or not. 
-#The syntax of the try-finally statement is this −
-
+1. This kind of a try-except statement catches all the exceptions that occur.
+2. Using this kind of try-except statement is not considered a good programming
+practice though, because it catches all exceptions but does not make the programmer
+Identify the root cause of the problem that may occur.
+==========================================================
+1. The try-finally Clause :
+2. You can use a finally: block along with a try: block.
+3. The finally block is a place to put any code that must execute, 
+whether the try-block raised an exception or not. 
+4. The syntax of the try-finally statement is this −
+==========================================================
 try:
    You do your operations here;
    ......................
@@ -71,19 +66,17 @@ try:
 finally:
    This would always be executed.
    ......................
-#You cannot use else clause as well along with a finally clause.
-
-
+You cannot use else clause as well along with a finally clause.
+==========================================================
 try:
    fh = open("testfile", "w")
    fh.write("This is my test file for exception handling!!")
 finally:
    print "Error: can\'t find file or read data"
-#If you do not have permission to open the file in writing mode,
-#then this will produce the following result −
-#Error: can't find file or read data
-
-
+If you do not have permission to open the file in writing mode,
+then this will produce the following result −
+Error: can't find file or read data
+==========================================================
 try:
    fh = open("testfile", "w")
    try:
@@ -93,17 +86,16 @@ try:
       fh.close()
 except IOError:
    print "Error: can\'t find file or read data"
-   
-
-#User-Defined Exceptions
-#Python also allows you to create your own exceptions by deriving 
-#classes from the standard built-in exceptions.
-#Here is an example related to RuntimeError. Here, a class is created 
-#that is subclassed from RuntimeError. This is useful when you need to 
-#display more specific information when an exception is caught.
-#In the try block, the user-defined exception is raised and caught in the 
-#except block. The variable e is used to create an instance of the class Networkerror.
-
+==========================================================   
+User-Defined Exceptions
+Python also allows you to create your own exceptions by deriving 
+classes from the standard built-in exceptions.
+Here is an example related to RuntimeError. Here, a class is created 
+that is subclassed from RuntimeError. This is useful when you need to 
+display more specific information when an exception is caught.
+In the try block, the user-defined exception is raised and caught in the 
+except block. The variable e is used to create an instance of the class Networkerror.
+==========================================================
 class Networkerror(RuntimeError):
    def __init__(self, arg):
       self.args = arg
@@ -168,108 +160,103 @@ ValueError		Raised when a function gets argument of correct type but improper va
 IOError			Raised when an input/ output operation fails
 RuntimeError		Raised when a generated error does not fall into any category
 =======================================================
-#Exceptions versus Syntax Errors :
-#Syntax errors occur when the parser detects an incorrect statement. Observe the following example:
-
+Exceptions versus Syntax Errors :
+Syntax errors occur when the parser detects an incorrect statement. Observe the following example:
+==========================================================
 >>> print( 0 / 0 ))
   File "<stdin>", line 1
     print( 0 / 0 ))
                   ^
-#SyntaxError: invalid syntax
-#The arrow indicates where the parser ran into the syntax error. 
-#In this example, there was one bracket too many. Remove it and run your code again:
-
+SyntaxError: invalid syntax
+The arrow indicates where the parser ran into the syntax error. 
+In this example, there was one bracket too many. Remove it and run your code again:
+==========================================================
 >>> print( 0 / 0)
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 ZeroDivisionError: integer division or modulo by zero
-
-#This time, you ran into an exception error. This type of error occurs whenever 
-#syntactically correct Python code results in an error. 
-#The last line of the message indicated what type of exception error you ran into.
-#Instead of showing the message exception error, Python details what type 
-#of exception error was encountered. In this case, it was a ZeroDivisionError.
-#Python comes with various built-in exceptions as well as the possibility to create self-defined exceptions.
-
-
-#Raising an Exception
-#We can use raise to throw an exception if a condition occurs.
-#The statement can be complemented with a custom exception.
-#If you want to throw an error when a certain condition occurs using raise, 
-#you could go about it like this:
-
+==========================================================
+This time, you ran into an exception error. This type of error occurs whenever 
+syntactically correct Python code results in an error. 
+The last line of the message indicated what type of exception error you ran into.
+Instead of showing the message exception error, Python details what type 
+of exception error was encountered. In this case, it was a ZeroDivisionError.
+Python comes with various built-in exceptions as well as the possibility to create self-defined exceptions.
+==========================================================
+Raising an Exception :
+We can use raise to throw an exception if a condition occurs.
+The statement can be complemented with a custom exception.
+If you want to throw an error when a certain condition occurs using raise, 
+you could go about it like this:
+==========================================================
 x = 10
 if x > 5:
     raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
 
 	When you run this code, the output will be the following:
-
 Traceback (most recent call last):
   File "<input>", line 4, in <module>
 Exception: x should not exceed 5. The value of x was: 10
-#The program comes to a halt and displays our exception to screen, 
-#offering clues about what went wrong.
-
-
-#The AssertionError Exception
-#Instead of waiting for a program to crash midway, you can also start by making an assertion in Python.
-# We assert that a certain condition is met. If this condition turns out to be True, then that
-# is excellent! The program can continue. If the condition turns out to be False, you can have the program 
-#throw an AssertionError exception.
-
-#Python assert statement :
-#Have a look at the following example, where it is asserted that the code will be executed on a Linux system:
+The program comes to a halt and displays our exception to screen, 
+offering clues about what went wrong.
+==========================================================
+The AssertionError Exception :
+Instead of waiting for a program to crash midway, you can also start by making an assertion in Python.
+We assert that a certain condition is met. If this condition turns out to be True, then that
+is excellent! The program can continue. If the condition turns out to be False, you can have the program 
+throw an AssertionError exception.
+==========================================================
+Python assert statement :
+Have a look at the following example, where it is asserted that the code will be executed on a Linux system:
 
 import sys
 assert ('linux' in sys.platform), "This code runs on Linux only."
-#If you run this code on a Linux machine, the assertion passes.
-#If you were to run this code on a Windows machine, the outcome of the
-#assertion would be False and the result would be the following:
+If you run this code on a Linux machine, the assertion passes.
+If you were to run this code on a Windows machine, the outcome of the
+assertion would be False and the result would be the following:
 
 Traceback (most recent call last):
   File "<input>", line 2, in <module>
 AssertionError: This code runs on Linux only.
 In this example, throwing an AssertionError exception is the last thing that the program will do. The program will come to halt and will not continue. What if that is not what you want?
+==========================================================
+The try and except Block: Handling Exceptions
+The try and except block in Python is used to catch and handle exceptions. 
+Python executes code following the try statement as a “normal” part of the program. 
+The code that follows the except statement is the program’s response to any exceptions 
+in the preceding try clause.
 
-#The try and except Block: Handling Exceptions
-#The try and except block in Python is used to catch and handle exceptions. 
-#Python executes code following the try statement as a “normal” part of the program. 
-#The code that follows the except statement is the program’s response to any exceptions 
-#in the preceding try clause.
-
-#As you saw earlier, when syntactically correct code runs into an error,
-#Python will throw an exception error. This exception error will crash the 
-#program if it is unhandled. The except clause determines how your program responds to exceptions.
-
-#The following function can help you understand the try and except block:
+As you saw earlier, when syntactically correct code runs into an error,
+Python will throw an exception error. This exception error will crash the 
+program if it is unhandled. The except clause determines how your program responds to exceptions.
+==========================================================
+The following function can help you understand the try and except block:
 
 def linux_interaction():
     assert ('linux' in sys.platform), "Function can only run on Linux systems."
     print('Doing something.')
-#The linux_interaction() can only run on a Linux system. 
-#The assert in this function will throw an AssertionError exception 
-#if you call it on an operating system other then Linux.
-#You can give the function a try using the following code:
-
+The linux_interaction() can only run on a Linux system. 
+The assert in this function will throw an AssertionError exception 
+if you call it on an operating system other then Linux.
+You can give the function a try using the following code:
+==========================================================
 try:
     linux_interaction()
 except:
     pass
+The way you handled the error here is by handing out a pass. 
+If you were to run this code on a Windows machine, you would get the following output:
 
-#The way you handled the error here is by handing out a pass. 
-#If you were to run this code on a Windows machine, you would get the following output:
-
-#Summing Up :
-#1.After seeing the difference between syntax errors and exceptions, you learned about 
-#various ways to raise, catch, and handle exceptions in Python. In this article, you saw the following options:
-#2.raise allows you to throw an exception at any time.
-#3.assert enables you to verify if a certain condition is met and throw an exception if it isn’t.
-#4.In the try clause, all statements are executed until an exception is encountered.
-#5.except is used to catch and handle the exception(s) that are encountered in the try clause.
-#6.else lets you code sections that should run only when no exceptions are encountered in the try clause.
-#7.finally enables you to execute sections of code that should always run, 
-#with or without any previously encountered exceptions.
-
+Summing Up :
+1. After seeing the difference between syntax errors and exceptions, you learned about 
+various ways to raise, catch, and handle exceptions in Python. In this article, you saw the following options:
+2. raise allows you to throw an exception at any time.
+3. assert enables you to verify if a certain condition is met and throw an exception if it isn’t.
+4. In the try clause, all statements are executed until an exception is encountered.
+5. except is used to catch and handle the exception(s) that are encountered in the try clause.
+6. else lets you code sections that should run only when no exceptions are encountered in the try clause.
+7. finally enables you to execute sections of code that should always run, 
+with or without any previously encountered exceptions.
 ========================================================
 try:
     print (1/0)
@@ -500,8 +487,7 @@ try:
         print("appropriate number")
 except ValueError:
         print("enter appropriate value")
-        
-#enter any numbergdfgdf
+        #enter any numbergdfgdf
 #enter appropriate value
 =============================================================
 try:
@@ -515,6 +501,3 @@ try:
         read_data = file.read()
 except:
     print('Could not open file.log')
-
-
-  
