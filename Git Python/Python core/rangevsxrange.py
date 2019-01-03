7b12af553aa19543e90e3e405391357b6973c81d
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-01-03 14:55:27 +0530

    Update rangevsxrange.py

--- Git Python/Python core/rangevsxrange.py
+++ Git Python/Python core/rangevsxrange.py
@@ -1,18 +1,18 @@
 #Range vs xrange 
-====================
-#range() vs xrange() in Python
-
-#range() and xrange() are two functions that could be used to 
-#iterate a certain number of times in for loops in Python. 
-#In Python 3, there is no xrange , but the range function behaves 
-#like xrange in Python 2.If you want to write code that will run on 
-#both Python 2 and Python 3, you should use range().
-
-#range() – This returns a list of numbers created using range() function.
-#xrange() – This function returns the generator object that can be used to 
-#display numbers only by looping. Only particular range is displayed on demand 
-#and hence called “lazy evaluation“.
-
+=================================
+range() vs xrange() in Python
+=================================
+1. range() and xrange() are two functions that could be used to 
+iterate a certain number of times in for loops in Python. 
+2. In Python 3, there is no xrange , but the range function behaves 
+3. like xrange in Python 2.
+4. If you want to write code that will run on both Python 2 and Python 3, you should use range().
+===============================
+1. range() – This returns a list of numbers created using range() function.
+2. xrange() – This function returns the generator object that can be used to 
+display numbers only by looping. Only particular range is displayed on demand 
+and hence called “lazy evaluation“.
+===============================
 #Both are implemented in different ways and have different characteristics associated with them.
 #The points of comparisons are:
 #Return Type
