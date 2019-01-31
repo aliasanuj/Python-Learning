889ab3b491fa18725408ac9c34e91ce47ea958c0
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-01-31 14:19:05 +0530

    Update difference.py

--- Git Python/Python core/difference.py
+++ Git Python/Python core/difference.py
@@ -57,5 +57,10 @@ from functools import reduce
 product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
 print(product)
 #24
-
+=============================
+from functools import reduce
+product = reduce(lambda x, y: x * y, [1, 2, 3, 4])
+print(product)
+#24
+==============================
 
