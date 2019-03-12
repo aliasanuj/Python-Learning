651870ae3583f17bd8c3768e061736f449e40c5c
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-12 18:45:01 +0530

    Update impt topic.py

--- Git Python/impt topic.py
+++ Git Python/impt topic.py
@@ -12,3 +12,31 @@ __str__() | __repr__()
 
 
 ===================
+switch case 
+break | continue | pass | 
+decorator(class | function)
+if __name__ == "__main__":
+yield 
+Monkey Patching in Python (Dynamic Behavior)
+where we can use multi threading in python
+zip vs 
+list comprehensive
+diff between array and list
+access specifier
+=====================================
+pandas 
+numpy
+scikit
+matplotlib
+=====================================
+Django
+Models
+Views
+Templates
+Session management
+files.py
+==============================
+mysql revision
+==============================
+JS
+==============================
