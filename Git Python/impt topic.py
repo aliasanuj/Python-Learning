992e8ab9379c1d26a62ac0d9feb1bfe5db8422da
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-12 18:43:46 +0530

    Create impt topic.py

---
+++ Git Python/impt topic.py
@@ -0,0 +1,14 @@
+Inbuilt functions :
+========================
+ascii
+bytearray
+bytes
+class method vs static method
+Python globals()
+execute vs compile
+memoryview()
+str() | repr()
+__str__() | __repr__()
+
+
+===================
