5ff5f848a56d0df3d562e47df0f4baa83ebd283d
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-14 11:53:06 +0530

    Update Links.py

--- Git Python/Python core/Links.py
+++ Git Python/Python core/Links.py
@@ -2,3 +2,6 @@
 
 https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
 
+#spotify icard link
+
+https://www.canva.com/design/DADT6TalWKg/76547GHtVcABeDL56erp_w/edit?layoutQuery=id&category=tACZCnZq6cQ&utm_source=onboarding
