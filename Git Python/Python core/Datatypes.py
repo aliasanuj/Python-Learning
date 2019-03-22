a1b158f7980951a4fe03d6f99f23254d7ee6f03f
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-03-22 10:47:28 +0530

    Create Datatypes.py

---
+++ Git Python/Python core/Datatypes.py
@@ -0,0 +1,20 @@
+immutable :
+===============
+int
+float
+decimal
+complex
+bool
+string
+tuple
+range
+frozenset
+bytes
+
+Mutable :
+=============
+list
+dictionary
+set
+bytearray
+user defined classes
