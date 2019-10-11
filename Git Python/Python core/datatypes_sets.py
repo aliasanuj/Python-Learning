a57e68f2e76462e5900354ad3ca2fa8411df809b
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-10-11 23:41:30 +0530

    Update datatypes_sets.py

--- Git Python/Python core/datatypes_sets.py
+++ Git Python/Python core/datatypes_sets.py
@@ -33,6 +33,19 @@ Output:
 #Frozen Set
 #frozenset(['e', 'g', 'f'])
 ========================================
+normal_set = set(["a", "b","c"])
+print(normal_set)
+normal_set.add("d")
+print(normal_set)
+frozen_set = frozenset(["e", "f", "g"])
+print(frozen_set)
+frozen_set.add("z")
+print(frozen_set)
+{'c', 'a', 'b'}
+{'c', 'd', 'a', 'b'}
+frozenset({'g', 'f', 'e'})
+#AttributeError: 'frozenset' object has no attribute 'add'
+========================================
 1. add(x) Method: Adds the item x to set if it is not already present in the set.
 people = {"Jay", "Idrish", "Archil"}
 people.add("Daxit") 
@@ -53,6 +66,7 @@ population = people.union(vampires)
 OR
 population = people|vampires
 #Set population set will have components of both people and vampire
+
 people = {"Jay", "Idrish", "Archil"}
 vampires = {"Karan", "Arjun"}
 population = people.union(vampires)
@@ -66,6 +80,7 @@ print(population1)
 #operator comes can also be used in this case.
 victims = people.intersection(vampires)
 -> Set victims will contain the common element of people and vampire
+
 people = {"Jay", "Idrish", "Archil"}
 vampires = {"Karan", "Arjun","Jay"} #case sensitive 'j' will not work
 population = people.intersection(vampires)
@@ -75,12 +90,22 @@ print(population1)
 #{'Jay'}
 #{'Jay'}
 =================================
+people = {"Jay", "Idrish", "Archil"}
+vampires = {"Karan", "Arjun","jay"} #case sensitive 'j' will not work
+population = people.intersection(vampires)
+print(population)
+population1 = people&vampires
+print(population1)
+set()
+set()
+=================================
 4. difference(s) Method: Returns a set containing all the elements of invoking
 # set but not of the second set. We can use ‘-‘ operator here.
 safe = people.difference(vampires)
 OR
 safe = people – vampires
 #-> Set safe  will have all the elements that are in people but not vampire
+
 people = {"Jay", "Idrish", "Archil"}
 vampires = {"Karan", "Arjun","Jay"} #case sensitive 'j' will not work
 population = people.difference(vampires)
@@ -90,34 +115,113 @@ print(population1)
 #{'Idrish', 'Archil'}
 #{'Idrish', 'Archil'}
 =================================
+people = {"Jay", "Idrish", "Archil"}
+vampires = {"Karan", "Arjun","Jay"} #case sensitive 'j' will not work
+population = people.difference(vampires)
+print(population)
+population1 = people-vampires
+print(population1)
+# {'Archil', 'Idrish'}
+# {'Archil', 'Idrish'}
+====================================
 5. clear() Method: Empties the whole set.
 victims.clear()
 -> Clears victim set
 However there are two major pitfalls in Python sets:
 The set doesn’t maintain elements in any particular order.
 Only instances of immutable types can be added to a Python set.
+
 people = {"Jay", "Idrish", "Archil"}
 vampires = {"Karan", "Arjun","Jay"} #case sensitive 'j' will not work
 population = people.clear()
 print(population)
 #None
 ============================================
-Method	Description
-add()	                  Adds an element to the set
-clear()	                Removes all the elements from the set
-copy()	                Returns a copy of the set
-difference()	          Returns a set containing the difference between two or more sets
-difference_update()	    Removes the items in this set that are also included in another, specified set
-discard()	              Remove the specified item
-intersection()	        Returns a set, that is the intersection of two other sets
-intersection_update()	  Removes the items in this set that are not present in other, specified set(s)
-isdisjoint()	          Returns whether two sets have a intersection or not
-issubset()	            Returns whether another set contains this set or not
-issuperset()	          Returns whether this set contains another set or not
-pop()	                  Removes an element from the set
-remove()	              Removes the specified element
-symmetric_difference()  Returns a set with the symmetric differences of two sets
+thisset = {"apple", "banana", "cherry"}
+thisset.update(["orange", "mango", "grapes"])
+print(thisset)
+#{'grapes', 'apple', 'banana', 'cherry', 'orange', 'mango'}
+============================================
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+#3
+============================================
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+#{'cherry', 'apple'}
+=============================================
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("abc")
+print(thisset)
+#    thisset.remove("abc")
+============================================
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("aaa")
+print(thisset)
+#{'apple', 'cherry', 'banana'}
+# if the items will not be there, it wont yhrow any error
+==============================================
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+# apple
+# {'banana', 'cherry'}
+=============================================
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+#NameError: name 'thisset' is not defined
+=============================================
+thisset = {"apple", "banana", "cherry"}
+del thisset["apple"]
+del thisset[0]
+print(thisset)
+#TypeError: 'set' object does not support item deletion
+#TypeError: 'set' object does not support item deletion
+============================================
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+#{'c', 1, 2, 3, 'a', 'b'}
+================================================
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, "b"}
+set3 = set1.union(set2)
+print(set3)
+#{1, 2, 'b', 'a', 'c'}
+================================================
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+#{1, 2, 3, 'b', 'c', 'a'}
+===============================================
+Note: Both union() and update() will exclude any duplicate items.
+===============================================
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+#{'apple', 'banana', 'cherry'}
+=================================================
+Method	Description :
+
+add()	                	  Adds an element to the set
+clear()	               		Removes all the elements from the set
+copy()	                	Returns a copy of the set
+difference()	        	  Returns a set containing the difference between two or more sets
+difference_update()	    	Removes the items in this set that are also included in another, specified set
+discard()	            	  Remove the specified item
+intersection()	        	Returns a set, that is the intersection of two other sets
+intersection_update()		  Removes the items in this set that are not present in other, specified set(s)
+isdisjoint()	        	  Returns whether two sets have a intersection or not
+issubset()	            	Returns whether another set contains this set or not
+issuperset()	        	  Returns whether this set contains another set or not
+pop()	                	  Removes an element from the set
+remove()	            	  Removes the specified element
+symmetric_difference()  	Returns a set with the symmetric differences of two sets
 symmetric_difference_update()	inserts the symmetric differences from this set and another
-union()	                 Return a set containing the union of sets
-update()	              Update the set with the union of this set and others
+union()	                 	Return a set containing the union of sets
+update()	              	Update the set with the union of this set and others
 =================================
