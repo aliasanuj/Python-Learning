5e549a6d4a0172715c8ad89d1b708811d50e6da7
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-10-12 22:08:41 +0530

    Update difference.py

--- Git Python/Python core/difference.py
+++ Git Python/Python core/difference.py
@@ -1,8 +1,8 @@
 Differences :
   
------------------------->
+========================>
 Generator vs iterator :
------------------------->
+========================>
 1. In creating a python generator, we use a function. But in creating an iterator in python, we use the iter() and next() functions.
 2. A generator in python makes use of the ‘yield’ keyword. A python iterator doesn’t.
 3. Python generator saves the states of the local variables every time ‘yield’ pauses the loop in python. An iterator does not 
@@ -14,20 +14,25 @@ iter() and next() functions.
 7. Generator in python let us write fast and compact code. This is an advantage over Python iterators. They are also simpler to 
 code than do custom iterator.
 
------------------------->
+========================>
 itertools vs functools
------------------------->
+========================>
 import itertools : itertools.accumulate : return list
 import functools : functools.reduce : return singleton value (not list)
 
------------------------->
+========================>
 #map vs filter
------------------------->
+========================>
 number_list = range(-5, 5)
 less_than_zero = list(filter(lambda x: x < 0, number_list))
 print(less_than_zero)
 #[-5, -4, -3, -2, -1]
 ========================
+number_list = range(-5, 5)
+less_than_zero = filter(lambda x: x < 0, number_list)
+print(less_than_zero)
+#<filter object at 0x0115DD90>
+===========================
 number_list = range(-5, 5)
 less_than_zero = list(map(lambda x: x < 0, number_list))
 print(less_than_zero)
