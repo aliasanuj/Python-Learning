cb9a67d26a860323407b2fa0f0a8ea38f412f162
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2019-11-01 12:33:52 +0530

    Update closure.py

--- Git Python/Python core/closure.py
+++ Git Python/Python core/closure.py
@@ -16,14 +16,42 @@ def gen_multiplier(x):
 obj = gen_multiplier(10)
 print(obj(5))
 #50
+===================================
+def gen_multiplier(x):
+
+    def multiplier(y):
+        return x * y
+    return multiplier
+
+    def multiplier(y):
+        return x + y
+    return multiplier
+
+obj = gen_multiplier(10)
+print(obj(5))
+#15 it wont call the second function. it will calll only immediate function 
 =======================
+def gen_multiplier(x):
+
+    def multiplier(y):
+        return x * y
+    return multiplier
+
+    def multiplier(y):
+        return x + y
+    return multiplier() #it wont reach here
+
+obj = gen_multiplier(10)
+print(obj(5))
+#50
+=========================
 def gen_multiplier(x):
     def multiplier(y):
         return x * y
     return multiplier()
 obj = gen_multiplier(10)
 print(obj(5))
-#error
+#TypeError: multiplier() missing 1 required positional argument: 'y'
 =========================
 def gen_multiplier():
     x = 10
@@ -80,6 +108,16 @@ obj(10)
 def outer(a):
     print(a)
     def inner(b):
+        print(a+b)
+    return inner
+obj = outer(5)
+obj(10)
+#5
+#15
+=============================
+def outer(a):
+    print(a)
+    def inner(b):
         print("aaa")
         print(b)
         print(a+b)
@@ -96,6 +134,19 @@ obj(10)
 # 10
 # 15
 ==============================
+def outer(a):
+    print(a)
+    def inner(b):
+        print("aaa")
+        print(b)
+        print(a+b)
+    return inner
+obj = outer(5)
+# print(obj(10))
+# obj(10)
+#op :
+#5
+==========================
 def gen_multiplier(a):
     x = 10 #non-local variable
     def multiplier(y): #stores the reference to non-local variable
@@ -122,6 +173,21 @@ print(obj(5))
 # 5
 # 500
 =========================
+def gen_multiplier(a):
+    x = 10 #non-local variable
+    print(a)
+    def multiplier(y): #stores the reference to non-local variable
+        print(y)
+        return x * y * a #x is non local to multiplier
+    return multiplier
+#call the function
+obj = gen_multiplier(12)
+del gen_multiplier #it will delete
+print(obj(5))
+# 12
+# 5
+# 600
+=======================
 m = 12
 def gen_multiplier(a):
     x = 10 #non-local variable
@@ -134,6 +200,23 @@ del gen_multiplier #it will delete
 print(obj(5))
 #6000
 ===========================
+global m
+m = 12
+def gen_multiplier(a):
+    m = 10
+    print(m)
+    print(a)
+    def multiplier(y): #stores the reference to non-local variable
+        return m * y * a #m will be 10 not 12
+    return multiplier
+#call the function
+obj = gen_multiplier(7)
+del gen_multiplier #it will delete
+print(obj(5))
+# 10
+# 7
+# 350
+==========================
 nested-function
 nested function should access non-local variable
 it should return the en-closed function
@@ -149,6 +232,15 @@ def print_msg(msg):
 print_msg("Hello")
 =======================================
 def print_msg(msg):
+    print("aaa")
+    def printer():
+        print(msg)
+    return printer()
+print_msg("Hello")
+# aaa
+# Hello
+=========================
+def print_msg(msg):
 # This is the outer enclosing function
     def printer():
 # This is the nested function
@@ -162,10 +254,24 @@ print_msg("Hello")
 def print_msg(msg):
     def printer():
         print(msg)
+    return printer # () is required
+print_msg("Hello")
+# no output
+======================
+def print_msg(msg):
+    def printer():
+        print(msg)
     return printer()
 print_msg("Hello")
 #Hello
 =====================================
+def print_msg(msg):
+    def printer(msg):
+        print(msg)
+    return printer()
+print_msg("Hello")
+#TypeError: printer() missing 1 required positional argument: 'msg'
+======================
 def outer(a1):
     def inner(a2):
         print(a1 + a2)
@@ -174,6 +280,30 @@ obj = outer(12)
 obj(15)
 #27
 ================================
+def outer(a1):
+    def inner(a2):
+        print(a1 + a2)
+    return inner
+print(outer(10))
+#<function outer.<locals>.inner at 0x036E9D68>
+========================
+def outer(a1):
+    def inner(a2):
+        print(a1 + a2)
+    return inner
+obj = outer(10)
+print(obj(5))
+# 15
+# None
+=====================
+def outer(a1):
+    def inner(a2):
+        print(a1 + a2)
+    return inner()
+obj = outer(10)
+print(obj(5))
+#TypeError: inner() missing 1 required positional argument: 'a2'
+==========================
 def print_msg(msg):
     def printer():
         print(msg)
@@ -218,6 +348,35 @@ print_msg("Hello")
 #Hello
 #We can see that the nested function printer() was able to access the non-local variable msg of the enclosing function.
 =================================
+def print_msg(msg):
+    def printer():
+        print(msg)
+    return printer()
+print_msg("aaa")
+#aaa
+==============================
+def print_msg(msg):
+    def printer():
+        print(msg)
+    return printer
+print_msg("aaa")
+#no output
+============================
+def print_msg(msg):
+    def printer():
+        print(msg)
+    return printer
+obj = print_msg("aaa")
+#no output
+=============================
+def print_msg(msg):
+    def printer():
+        print(msg)
+    return printer
+obj = print_msg("aaa")
+print(obj("aac"))
+#TypeError: printer() takes 0 positional arguments but 1 was given
+==============================
 #Defining a Closure Function
 def print_msg(msg):
 # This is the outer enclosing function
@@ -261,6 +420,21 @@ def outer_func():
   def inner_func():
     z = 'local'
     print(x, y, z)
+    def aaa():
+        print(z)
+    return  aaa()
+  return inner_func()
+print(outer_func())
+# global enclose local
+# local
+# None
+================================
+x = 'global'
+def outer_func():
+  y = 'enclose'
+  def inner_func():
+    z = 'local'
+    print(x, y, z)
   inner_func()
 outer_func()
 print(outer_func())
@@ -268,6 +442,18 @@ print(outer_func())
 #global enclose local
 #None
 ================================
+x = 'global'
+def outer_func():
+  y = 'enclose'
+  def inner_func():
+    z = 'local'
+    print(x, y, z)
+  inner_func()
+print(outer_func())
+# global enclose local
+# None
+
+===============================
 def outer_func():
   x = 5
   def inner_func(y = 3):
